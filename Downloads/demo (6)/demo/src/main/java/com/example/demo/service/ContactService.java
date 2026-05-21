a2d4a232980cc7e998439bb52d3e75f018913a0d
Revaldo007 <revaldoambrose90@gmail.com>
2026-05-21 09:50:55 +0530

    Delete Downloads/demo (6)/demo directory

--- Downloads/demo (6)/demo/src/main/java/com/example/demo/service/ContactService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.dto.ContactRequestDTO;
-import com.example.demo.dto.ContactResponseDTO;
-
-public interface ContactService {
-    ContactResponseDTO saveMessage(ContactRequestDTO request);
-}
